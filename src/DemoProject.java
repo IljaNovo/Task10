1212561685682e6ea01df0bc2fc429c306abf1da
Ilya Novozhilov <ilja.keinn@mail.ru>
2016-01-24 19:13:44 +0300

    mission 2 accomplished

--- src/DemoProject.java
+++ src/DemoProject.java
@@ -1,10 +1,11 @@
 import java.io.FileNotFoundException;
+import java.util.Collection;
 import java.util.Map;
 
 public class DemoProject {
 
     public static void main(String[] args) {
-        // Task 1 theme: IOStream, read and write
+
 
         ParserParams parParams = new ParserParams();
         Map<String, String> namesFiles = null;
@@ -15,10 +16,30 @@ public class DemoProject {
             raw.openStreamReader("UTF16", "src\\" + namesFiles.get("Read"));
             raw.openStreamWriter("cp1251", "src\\" + namesFiles.get("Write"));
 
-            raw.write(raw.read());
+            // Task 1 theme: IOStream, read and write //
+                                                      //
+            raw.write(raw.read());                    //
+            raw.closeStreamReader();                  //
+            raw.closeStreamWriter();                  //
+                                                      //
+            ////////////////////////////////////////////
 
-            raw.closeStreamReader();
-            raw.closeStreamWriter();
+            // Task 2 theme: IOStream, read English text ////////////////////////////
+                                                                                   //
+            ReaderAndWriter rawFirst = new ReaderAndWriter();                      //
+            rawFirst.openStreamReader("UTF8", "src\\" + namesFiles.get("Read"));   //
+                                                                                   //
+            EnglishText et = new EnglishText(rawFirst.read());                     //
+                                                                                   //
+            Map<String, Integer> answer = et.frequency();                          //
+            Collection<String> keys = answer.keySet();                             //
+                                                                                   //
+            for (String key : keys) {                                              //
+                System.out.printf("%s - %d%n", key, answer.get(key));              //
+            }                                                                      //
+            rawFirst.closeStreamReader();                                          //
+                                                                                   //
+            /////////////////////////////////////////////////////////////////////////
         }
         catch (ParseCommandLineException e) {
             e.getStackTrace();
@@ -35,9 +56,5 @@ public class DemoProject {
         catch (FileNotFoundException e) {
             e.getStackTrace();
         }
-
-        // Task 2 theme: IOStream, read English text
-
-        
     }
 }
